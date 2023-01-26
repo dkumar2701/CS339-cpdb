692b1809b4ad12bff966e8a4567ab2e07f37d500
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-26 14:47:05 -0600

    changed after to on or after

--- sql/01.sql
+++ sql/01.sql
@@ -1,4 +1,10 @@
+/*
+ Give the names of the officers who joined on or after March 15, 2020.
+ Please give their first name, middle initial, and last name and sort them by last name using their first name
+ in the event of a tie.
+ */
+
 SELECT d.first_name, d.middle_initial, d.last_name
 FROM data_officer d
-WHERE d.appointed_date > '3/15/2020'
+WHERE d.appointed_date >= '3/15/2020'
 ORDER BY d.last_name, d.first_name
